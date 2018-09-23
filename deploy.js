20f32b8130a5ba4bcc292f2958e8327a61cb1e96
Krystof Beuermann <kreiselkreisch@gmx.de>
2018-09-23 13:19:40 +0200

    change design and deployment

---
+++ deploy.js
@@ -0,0 +1,20 @@
+const FtpDeploy = require("ftp-deploy");
+const ftpDeploy = new FtpDeploy();
+require("dotenv").config();
+
+const config = {
+  user: process.env.user,
+  password: process.env.password,
+  host: "praxis-kreativkai.de",
+  port: 21,
+  localRoot: __dirname + "/build",
+  remoteRoot: "/praxis-kreativkai.de/",
+  include: ["*", "**/*", ".htaccess"], // this uploads everything except dot files
+  deleteRemote: true // delete existing files at destination before uploading
+};
+
+// use with promises
+ftpDeploy
+  .deploy(config)
+  .then(res => console.log("finished"))
+  .catch(err => console.log(err));
